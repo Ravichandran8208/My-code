b79667e49e7720caa89f29262b5feae722433013
Ravichandran8208 <95357928+Ravichandran8208@users.noreply.github.com>
2022-06-06 15:59:54 +0530

    Add files via upload

---
+++ MyFrameWork/src/test/java/testcasesPOM/VerifyPages.java
@@ -0,0 +1,24 @@
+package testcasesPOM;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import baseClassPOM.EELoginCredentialsBase;
+import pagesPOM.Loginpage;
+
+public class VerifyPages extends EELoginCredentialsBase {
+	@BeforeTest
+	public void set() {
+		
+
+	}
+	@Test(dataProvider = "fetchData")
+
+	public void testingpages(String unitcode,String Userid ,String password ) throws InterruptedException {
+		Loginpage lp=new Loginpage((ChromeDriver) driver);
+		lp.enterUnitCode(unitcode).Userid(Userid).enterPassword(password).clickSubmit().clickImportLetter();
+
+	}
+
+}
